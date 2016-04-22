024de979c734ecf528527ce0f43436220874dc3a
lhg <395900196@qq.com>
2016-04-22 11:34:25 +0800

    ymcms init

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-04-22 00:12:27 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-04-22T00:12:27+00:00	INFO 127.0.0.1	joomlafailure	用户名和密码不吻合或者您尚未注册一个帐号。
